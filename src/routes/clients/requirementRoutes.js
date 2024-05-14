dee7a881a1f58335d6e32a18cc42958ad6c27ef8
erikaliane <erika.ventura@tecsup.edu.pe>
2024-05-14 12:52:33 -0500

    requirement

--- src/routes/clients/requirementRoutes.js
+++ src/routes/clients/requirementRoutes.js
@@ -3,22 +3,9 @@ const router = express.Router();
 const multer = require('multer');
 const requirementController = require('../../controllers/clients/requirementController');
 const authenticateToken = require('../../middlewares/authenticationMiddleware');
-const cloudinary = require('cloudinary');
-require('dotenv').config();
-
-cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_SECRET,
-    secure: true
-});
-
-const storage = multer.memoryStorage(); // Define storage antes de configurar multer
-const upload = multer({ storage: storage });
 
 router.get('/', authenticateToken, requirementController.getAllRequirements);
-router.post('/', requirementController.createRequirement);
-router.post('/upload',  [authenticateToken, upload.single('image')], requirementController.uploadImage);
+router.post('/', authenticateToken,requirementController.createRequirement);
 
 
 
--- src/services/clients/requirementService.js
+++ src/services/clients/requirementService.js
@@ -3,37 +3,11 @@ const Profile = require('../../models/profileModel');
 const Requirement = require('../../models/requirementModel');
 const User = require('../../models/userModel');
 const Campaign = require('../../models/campaignModel');
-const cloudinary = require('cloudinary');
 
-cloudinary.v2.config({
-  cloud_name: 'dk2red18f',
-  api_key: '647789177635785',
-  api_secret: 'xa2vaybRQMfxna7Gd2oqrQg8eUg',
-  secure: true,
-});
-
-
-
-exports.uploadImages = async (images) => {
-  try {
-    const urls = [];
-    if (!Array.isArray(images)) {
-      // Si `images` no es un array, se asume que solo hay una imagen y se convierte en un array
-      images = [images];
-    }
-    for (const image of images) {
-      const result = await cloudinary.uploader.upload(image);
-      urls.push(result.secure_url);
-    }
-    return urls;
-  } catch (error) {
-    throw error;
-  }
-};
 
 
 // Função para criar um novo requirement no banco de dados
-exports.createRequirement = async (data) => {
+exports.create = async (data) => {
   try {
     const requirement = await Requirement.create(data);
     return requirement;
